c93c75eeeedd8eaaf30f3c7aa1a57f20d6efdb9b
Ruslan Kolosovskyi <ruslan.kolosovskyi@gmail.com>
2022-08-19 17:28:20 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NoticeObserveKit",
-            targets: ["NoticeObserveKit_iOS"]),
+            targets: ["NoticeObserveKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,13 +22,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "NoticeObserveKit_iOS",
+            name: "NoticeObserveKit",
             dependencies: [],
             path: "NoticeObserveKit",
             exclude: ["Info.plist"]),
         .testTarget(
             name: "NoticeObserveKitTests",
-            dependencies: ["NoticeObserveKit_iOS"],
+            dependencies: ["NoticeObserveKit"],
             path: "NoticeObserveKitTests",
             exclude: ["Info.plist"]),
     ]
