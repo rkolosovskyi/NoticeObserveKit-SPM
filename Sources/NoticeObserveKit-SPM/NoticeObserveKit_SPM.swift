3bc8f451f77a29f3b6091f1d80ca4e565544473d
Ruslan Kolosovskyi <ruslan.kolosovskyi@gmail.com>
2022-08-19 17:19:29 +0300

    SPM support has been added (for iOS).

---
+++ Sources/NoticeObserveKit-SPM/NoticeObserveKit_SPM.swift
@@ -0,0 +1,6 @@
+public struct NoticeObserveKit_SPM {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/NoticeObserveKit-SPMTests/NoticeObserveKit_SPMTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NoticeObserveKit_SPM
+
+final class NoticeObserveKit_SPMTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NoticeObserveKit_SPM().text, "Hello, World!")
+    }
+}
